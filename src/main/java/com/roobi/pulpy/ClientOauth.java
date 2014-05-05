cbd28ad1258895cbbc21afadcc095515776648bb
susee <suseemukil@gmail.com>
2014-05-05 15:21:16 +0530

    test1

---
+++ src/main/java/com/roobi/pulpy/ClientOauth.java
@@ -0,0 +1,103 @@
+package com.roobi.pulpy;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.Map;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.mindots.util.Utils;
+
+public class ClientOauth extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+
+    public ClientOauth() {
+        super();
+    }
+
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	}
+
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		Connection con=null;  
+	    PrintWriter out=response.getWriter();
+		String appid=request.getParameter("appid");
+	    String url=request.getParameter("url");
+		 Map<String, String> config = Utils.getConfigFromFile(getServletContext(), "config.properties");
+
+
+	    //String appid="MP_199";
+		try{
+		    Class.forName("com.mysql.jdbc.Driver").newInstance();
+            con = (Connection) DriverManager.getConnection(config.get("URL"),config.get("USER"),config.get("PASS"));
+		    PreparedStatement st=con.prepareStatement("SELECT * From authen t1 where t1.id=?");
+		    st.setString(1, appid);
+	        ResultSet rs = st.executeQuery();
+	        while(rs.next()){ 
+	         String id=rs.getString("id");
+	         HttpSession session=request.getSession(true);
+             session.setAttribute("id", id);
+             
+	   	     String authen1=rs.getString("auth");
+	   	     String appname=rs.getString("appname");
+	         String cname1=rs.getString("cname");
+           	 String ckey1=rs.getString("ckey");
+           	 String csecname1=rs.getString("csecname");
+           	 String cseckey1=rs.getString("cseckey");
+           	 String sname1=rs.getString("sname");
+           	 String svalue1=rs.getString("svalue");
+           	 String aurl1=rs.getString("aurl");
+           	 String tokenurl1=rs.getString("tokenurl");
+           	 String tlabel1=rs.getString("tlabel");
+           	 String treplace1=rs.getString("treplace");
+           	 String el1=rs.getString("el");
+           	 String ev1=rs.getString("ev");
+           	 String rf1=rs.getString("rf");
+           	 String rmethod1=rs.getString("rmethod");
+           	session.setAttribute("url", url);
+           	session.setAttribute("appname", appname);
+           	session.setAttribute("cname", cname1);
+    	    session.setAttribute("ckey", ckey1);
+    	    session.setAttribute("csecname", csecname1);
+    	    session.setAttribute("cseckey", cseckey1);
+    	    session.setAttribute("sname", sname1);
+    	    session.setAttribute("svalue", svalue1);
+    	    session.setAttribute("aurl", aurl1);
+    	    session.setAttribute("tokenurl", tokenurl1);
+    	    session.setAttribute("tlabel", tlabel1);
+    	    session.setAttribute("treplace", treplace1);
+    	    session.setAttribute("el", el1);
+    	    session.setAttribute("ev", ev1);
+    	    session.setAttribute("rf1", rf1);
+    	    session.setAttribute("rm1", rmethod1);
+           	 String clienturl=null;
+        	 if(sname1.equals("") && el1.equals(""))
+        		  clienturl=aurl1+"?redirect_uri=/OauthCallBackServlet&response_type=code&client_id="+ckey1;
+             else if(!sname1.equals("")&& el1.equals(""))
+        		  clienturl=aurl1+"?redirect_uri=/OauthCallBackServlet&response_type=code&client_id="+ckey1+"&"+sname1+"="+svalue1;
+             else if(!sname1.equals("")&& !el1.equals(""))
+        		  clienturl=aurl1+"?redirect_uri=/OauthCallBackServlet&response_type=code&client_id="+ckey1+"&"+sname1+"="+svalue1+"&"+el1+"="+ev1;
+             else if(sname1.equals("")&& !el1.equals(""))
+        		  clienturl=aurl1+"?redirect_uri=/OauthCallBackServlet&response_type=code&client_id="+ckey1+"&"+el1+"="+ev1;
+        	 response.setContentType("text/plain");
+        	 response.setCharacterEncoding("UTF-8");
+             response.getWriter().print(clienturl);
+
+	        }
+
+	}
+		catch(Exception e){}
+
+}
+}
--- src/main/java/com/roobi/pulpy/OauthCallBackServlet.java
+++ src/main/java/com/roobi/pulpy/OauthCallBackServlet.java
@@ -1,9 +1,15 @@
 package com.roobi.pulpy;
 
+import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.io.StringReader;
 
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -16,6 +22,8 @@ import org.apache.commons.httpclient.HttpClient;
 import org.apache.commons.httpclient.NameValuePair;
 import org.apache.commons.httpclient.methods.GetMethod;
 import org.apache.commons.httpclient.methods.PostMethod;
+import org.apache.commons.httpclient.util.URIUtil;
+import org.apache.http.client.methods.HttpPost;
 import org.json.JSONObject;
 import org.scribe.builder.ServiceBuilder;
 import org.scribe.model.OAuthConstants;
@@ -39,86 +47,129 @@ public class OauthCallBackServlet extends HttpServlet {
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		try{
-		PrintWriter pw=response.getWriter();
-		HttpSession session1=request.getSession(true);
-		String apilabel=(String) session1.getAttribute("cname");
- 		String apikey=(String) session1.getAttribute("ckey");
- 		String apiseclabel=(String) session1.getAttribute("csecname");
- 		String apisecvalue=(String) session1.getAttribute("cseckey");
- 		String scope_label=(String) session1.getAttribute("sname");
- 		String scope_value=(String) session1.getAttribute("svalue");
- 		String aurl=(String) session1.getAttribute("aurl");
- 		String tokenurl=(String) session1.getAttribute("tokenurl");
- 		String tlabel=(String) session1.getAttribute("tlabel");
- 		String treplace=(String) session1.getAttribute("treplace");
- 		String el=(String) session1.getAttribute("el");
- 		String ev=(String) session1.getAttribute("ev");
- 		String rf1=(String)session1.getAttribute("rf1");
- 		String rm1=(String)session1.getAttribute("rm1");
-		String code = request.getParameter(OAuthConstants.CODE);
-		//pw.println(apikey+"<br>"+apisecvalue+"<br>"+tokenurl);
-		System.out.println("verifier:"+code);
-		String responseBody=null;
-		String access_token=null;
-		HttpClient httpclient = new HttpClient();
-			 if(rm1.equals("POST")){
-			PostMethod post = new PostMethod(tokenurl);
-			post.addParameter("code", code);
-			post.addParameter("grant_type", "authorization_code");
-			post.addParameter("client_id",apikey);
-			post.addParameter("client_secret",apisecvalue);
-			post.addParameter("redirect_uri","https://mind-inputs.rhcloud.com/OauthCallBackServlet");
-			httpclient.executeMethod(post);
-		    responseBody = post.getResponseBodyAsString();}
-		  //  pw.println(responseBody);}
-		    
-			 else if(rm1.equals("GET")){
-			     	GetMethod get=new GetMethod(tokenurl+"?code="+code+"&grant_type=authorization_code&client_id="+apikey+"&client_secret="+apisecvalue+"&redirect_uri=http://localhost:8080/MindPulpy1/OauthCallBackServlet");
+			//	response.setContentType("application/json");
+			PrintWriter pw=response.getWriter();
+			HttpSession session1=request.getSession(true);
+			String url=(String) session1.getAttribute("url");
+			String id=(String) session1.getAttribute("id");
+			String appname=(String) session1.getAttribute("appname");
+			String apilabel=(String) session1.getAttribute("cname");
+	 		String apikey=(String) session1.getAttribute("ckey");
+	 		String apiseclabel=(String) session1.getAttribute("csecname");
+	 		String apisecvalue=(String) session1.getAttribute("cseckey");
+	 		String scope_label=(String) session1.getAttribute("sname");
+	 		String scope_value=(String) session1.getAttribute("svalue");
+	 		String aurl=(String) session1.getAttribute("aurl");
+	 		String tokenurl=(String) session1.getAttribute("tokenurl");
+	 		String tlabel=(String) session1.getAttribute("tlabel");
+	 		String treplace=(String) session1.getAttribute("treplace");
+	 		String el=(String) session1.getAttribute("el");
+	 		String ev=(String) session1.getAttribute("ev");
+	 		String rf1=(String)session1.getAttribute("rf1");
+	 		String rm1=(String)session1.getAttribute("rm1");
+			String code = request.getParameter(OAuthConstants.CODE);
+			//pw.println(apikey+"<br>"+apisecvalue+"<br>"+tokenurl);
+			System.out.println("verifier:"+code);
+			String responseBody=null;
+			String access_token=null;
+			HttpClient httpclient = new HttpClient();
+			if(rm1.equals("POST")){
+				PostMethod post = new PostMethod(tokenurl);
+				post.setRequestHeader("Accept", "application/json");
+				post.addParameter("code", code);
+				post.addParameter("grant_type", "authorization_code");
+				post.addParameter("client_id",apikey);
+				post.addParameter("client_secret",apisecvalue);
+				post.addParameter("redirect_uri","/OauthCallBackServlet");
+				httpclient.executeMethod(post);
+			    responseBody = post.getResponseBodyAsString();//}
+			    System.out.println(responseBody);
+				
+			    
+			}
+			  
+			    
+				 else if(rm1.equals("GET")){
+				     	GetMethod get=new GetMethod(URIUtil.encodeQuery(tokenurl+"?code="+code+"&grant_type=authorization_code&client_id="+apikey+"&client_secret="+apisecvalue+"&redirect_uri=http://localhost:8080/MindPulpy1/OauthCallBackServlet"));
 
-				/* get.setQueryString(new NameValuePair[] { 
-		     			    new NameValuePair("code",code)});
-				 get.setQueryString(new NameValuePair[] { 
-		     			    new NameValuePair("grant_type","authorization_code")});
-				 get.setQueryString(new NameValuePair[] { 
-		     			    new NameValuePair("client_id",apikey)});
-				 get.setQueryString(new NameValuePair[] { 
-		     			    new NameValuePair("client_secret",apisecvalue)});
-				 get.setQueryString(new NameValuePair[] { 
-		     			    new NameValuePair("redirect_uri","http://localhost:8080/MindPulpy1/OauthCallBackServlet")});*/
-		     		
-			         httpclient.executeMethod(get);
-			         responseBody=get.getResponseBodyAsString();}
-				 
-			 JSONObject json = null;
-				json = new JSONObject(responseBody);
-		        access_token = json.getString("access_token");
-			 
+
+				     	
+					/* get.setQueryString(new NameValuePair[] { 
+			     			    new NameValuePair("code",code)});
+					 get.setQueryString(new NameValuePair[] { 
+			     			    new NameValuePair("grant_type","authorization_code")});
+					 get.setQueryString(new NameValuePair[] { 
+			     			    new NameValuePair("client_id",apikey)});
+					 get.setQueryString(new NameValuePair[] { 
+			     			    new NameValuePair("client_secret",apisecvalue)});
+					 get.setQueryString(new NameValuePair[] { 
+			     			    new NameValuePair("redirect_uri","http://localhost:8080/MindPulpy1/OauthCallBackServlet")});*/
+			     		
+				          httpclient.executeMethod(get);
+				          responseBody=get.getResponseBodyAsString();
+						  System.out.println(responseBody);
+	}
 			
-		//Token accessToken = service.getAccessToken(OAuthConstants.EMPTY_TOKEN,new Verifier(code));
-		   //pw.println("Got Token:"+responseBody);  
-		          // if(rm1.equals("JSON")){
-//}
-		   		    /* else if(rm1.equals("XML")){
-		   		    	DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-		   		    	DocumentBuilder builder = factory.newDocumentBuilder();
-		   		    	org.w3c.dom.Document document = builder.parse(new InputSource(new StringReader(responseBody)));  
-		   		    	NodeList nl = document.getElementsByTagName("access_token"); 
-		                Node n = nl.item(0);
-		   		    	access_token = n.getFirstChild().getNodeValue(); // in some environments safer way
+			             String line=null;
+			             BufferedReader br=new BufferedReader(new StringReader(responseBody));
+			             while ((line = br.readLine()) != null) {
+			            	 if(line.startsWith("{") || line.startsWith("[{")){
+			            		 JSONObject json = null;
+			     				 json = new JSONObject(responseBody);
+			     		         access_token = json.getString("access_token"); 
+			            	 }
+			            	 else if(line.startsWith("<?") || line.endsWith("?>")){
+			            		    DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+					   		    	DocumentBuilder builder = factory.newDocumentBuilder();
+					   		    	org.w3c.dom.Document document = builder.parse(new InputSource(new StringReader(responseBody)));  
+					   		    	NodeList nl = document.getElementsByTagName("access_token"); 
+					                Node n = nl.item(0);
+					   		    	access_token = n.getFirstChild().getNodeValue();
+			            	 }
+			            	 else{
+			            	 String[] pairs = line.split("&");
+			                 for (String pair : pairs) {
+			                     String[] kv = pair.split("=");
+			                     if (kv.length != 2) {
+			                         throw new RuntimeException("Unexpected auth response");
+			                     } 
+			                     else {
+			                         if (kv[0].equals("access_token")) {
+			                             access_token = kv[1];
+			                         }}
+			                         
+			                 } }
+			                 }
+				
+				
+					   session1.setAttribute("access_token", access_token);
+					   session1.setAttribute("id", id);
+					   if(access_token.equals(""))
+				            {pw.println("<br><br><center><b><h2><font color='green'>Authentication Error with "+appname+"</font></center></h2></b>");}
+					   else{
+					    response.setContentType("text/html;charset=utf-8");
+			        	response.setCharacterEncoding("UTF-8");
+			            pw.println("<br><br><center><b><h2><font color='green'>Sucessfully Authenticated with "+appname+"</font></center></h2></b>");
+			            pw.println("<br><br><h3><center><a href='auth.jsp'>Continue with Config</a></center></h3>");
+			            pw.println("<br><br><h3><center><a href='token.jsp'>Continue with App</a></center></h3>");}
+			            /*ServletContext context = this.getServletContext();
+			            RequestDispatcher dispatcher = context.getRequestDispatcher("/token.jsp");
 
-		   		     }*/
+			            // change your request and response accordingly
 
-		            session1.setAttribute("access_token", access_token);	    
-		            request.getRequestDispatcher("auth.jsp").include(request, response);
-		        
+			            dispatcher.forward(request, response);*/
+
+			     //   response.sendRedirect("token.jsp");
+			
+
+			}
+			catch(Exception e){}
+
 		
-
-		}
-		catch(Exception e){}
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-	
+
 	}
 
 }
