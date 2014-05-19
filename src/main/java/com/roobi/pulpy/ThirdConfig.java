51f406cd2572ef643aab4411cca56b696188cfad
Durga Ashok <Durga Ashok@192.168.56.1>
2014-05-19 11:11:44 +0530

    nb n

--- src/main/java/com/roobi/pulpy/ThirdConfig.java
+++ src/main/java/com/roobi/pulpy/ThirdConfig.java
@@ -70,7 +70,7 @@ public class ThirdConfig extends HttpServlet {
    	        try {
             Class.forName("com.mysql.jdbc.Driver").newInstance();
             con = (Connection) DriverManager.getConnection(config.get("URL"),config.get("USER"),config.get("PASS"));
-            PreparedStatement st=con.prepareStatement("insert into thirdconfig(id,resf3,appid,thrdurl,thrdcycle,alabel,akey,thrd1,thrd2,t1,tv1,t2,tv2,t3,tv3,t4,tv4,t5,tv5,t6,tv6,t7,tv7,t8,tv8,t9,tv9,t10,tv10) values ('"+id+"','"+respf3+"','"+appid+"','"+thirdurl+"','"+thirdcycle+"','"+al+"','"+ak+"','"+thrd1+"','"+thrd2+"','"+t1+"','"+tv1+"','"+t2+"','"+tv2+"','"+t3+"','"+tv3+"','"+t4+"','"+tv4+"','"+t5+"','"+tv5+"','"+t6+"','"+tv6+"','"+t7+"','"+tv7+"','"+t8+"','"+tv8+"','"+t9+"','"+tv9+"','"+t10+"','"+tv10+"')");                
+            PreparedStatement st=con.prepareStatement("insert into thirdconfig(id,appid,thrdurl,thrdcycle,alabel,akey,thrd1,thrd2,t1,tv1,t2,tv2,t3,tv3,t4,tv4,t5,tv5,t6,tv6,t7,tv7,t8,tv8,t9,tv9,t10,tv10,resf3) values ('"+id+"','"+appid+"','"+thirdurl+"','"+thirdcycle+"','"+al+"','"+ak+"','"+thrd1+"','"+thrd2+"','"+t1+"','"+tv1+"','"+t2+"','"+tv2+"','"+t3+"','"+tv3+"','"+t4+"','"+tv4+"','"+t5+"','"+tv5+"','"+t6+"','"+tv6+"','"+t7+"','"+tv7+"','"+t8+"','"+tv8+"','"+t9+"','"+tv9+"','"+t10+"','"+tv10+"','"+respf3+"')");                
             st.executeUpdate();
             st.close();
             out.println("INsert sucess");
@@ -85,7 +85,7 @@ public class ThirdConfig extends HttpServlet {
              String oriapilabel=rs.getString("a1");String oriapikey=rs.getString("a2"); 
              
              String rf1=rs.getString("rf");String rm1=rs.getString("rm");
-             String resf1=rs.getString("resf1");//String eurl=rs.getString("endurl");
+             String resf1=rs.getString("resf");//String eurl=rs.getString("endurl");
              /*String tp1=rs.getString("p1");String tpv1=rs.getString("pv1");
              String tp2=rs.getString("p2");String tpv2=rs.getString("pv2");  // retrieve all valued from config table
              String tp3=rs.getString("p3");String tpv3=rs.getString("pv3");
