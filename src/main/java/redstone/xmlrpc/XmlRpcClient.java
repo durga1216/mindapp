5d796b2ca74f714485aabc99546e204f261217ef
Durga Ashok <Durga Ashok@192.168.56.1>
2014-06-06 15:58:05 +0530

    gvgtjv

--- src/main/java/redstone/xmlrpc/XmlRpcClient.java
+++ src/main/java/redstone/xmlrpc/XmlRpcClient.java
@@ -37,7 +37,7 @@ import org.xml.sax.SAXException;
 /**
  *  An XmlRpcClient represents a connection to an XML-RPC enabled server. It
  *  implements the XmlRpcInvocationHandler so that it may be used as a relay
- *  to other XML-RPC servers when installed in an XmlRpcServer. 
+ *  to other XML-RP
  *
  *  @author Greger Olsson
  */
