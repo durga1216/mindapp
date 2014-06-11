a07202844b247a2fa0a4da3cc215ddb14e82ef0e
Durga Ashok <Durga Ashok@durga>
2014-06-11 18:51:01 +0530

    yhn

--- src/main/java/com/roobi/pulpy/AuthPulpy.java
+++ src/main/java/com/roobi/pulpy/AuthPulpy.java
@@ -151,7 +151,7 @@ public class AuthPulpy extends HttpServlet {
 	         if(authen1.equals("No Auth")){
 	        	 Object obj;
 	        	 String str=null;
-	        	 if(rf1.equals("REST") && rm1.equals ("GET") && resf1.equals("XML") || resf1.equals("JSON")){ // No Auth XML get
+	        	 if(rf1.equals("REST") && rm1.equals ("GET")){ // No Auth XML get
 	        		 
             	   if(!"null".equals(pa1) && !"null".equals(pa2) && !"null".equals(pa3) && !"null".equals(pa4) && !"null".equals(pa5) && !"null".equals(pa6) && !"null".equals(pa7) && !"null".equals(pa8) && !"null".equals(pa9) && !"null".equals(pa10)){
 		        		 eurl=endurl1+"?"+pa1+"="+pva1+"&"+pa2+"="+pva2+"&"+pa3+"="+pva3+"&"+pa4+"="+pva4+"&"+pa5+"="+pva5+"&"+pa6+"="+pva6+"&"+pa7+"="+pva7+"&"+pa8+"="+pva8+"&"+pa9+"="+pva9+"&"+pa10+"="+pva10;}
