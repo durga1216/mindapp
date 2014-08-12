c6b8337f844d1167d32bb9be89b72bf425a1ef2e
susee <suseemukil@gmail.com>
2014-08-12 13:24:50 +0530

    fdsfsad

--- src/main/java/com/roobi/pulpy/AuthXmlPulpy.java
+++ src/main/java/com/roobi/pulpy/AuthXmlPulpy.java
@@ -514,7 +514,7 @@ public class AuthXmlPulpy extends HttpServlet {
        
        //Basic Authentication
 	        	 else if(authen1.equals("Signed Auth")){  //API Keys
-		        	 if(rf1.equals("REST") && rm1.equals ("GET") && resf1.equals("XML") || resf1.equals("JSON")){  //API XML get
+	        		 if(rf1.equals("REST") && rm1.equals ("GET") && resf1.equals("XML") || resf1.equals("JSON")){  //API XML get
 		        		 
 		        		 if(!"null".equals(pa1) && !"null".equals(pa2) && !"null".equals(pa3) && !"null".equals(pa4) && !"null".equals(pa5) && !"null".equals(pa6) && !"null".equals(pa7) && !"null".equals(pa8) && !"null".equals(pa9) && !"null".equals(pa10)){
 			        		 eurl=pa1+"="+p1+"&"+pa2+"="+p2+"&"+pa3+"="+p3+"&"+pa4+"="+p4+"&"+pa5+"="+p5+"&"+pa6+"="+p6+"&"+pa7+"="+p7+"&"+pa8+"="+p8+"&"+pa9+"="+p9+"&"+pa10+"="+p10;}
