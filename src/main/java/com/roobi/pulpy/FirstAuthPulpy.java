2e400da4243f58869be9086111dc4995dc58036b
susee <suseemukil@gmail.com>
2014-08-19 14:53:37 +0530

    sfddfds

--- src/main/java/com/roobi/pulpy/FirstAuthPulpy.java
+++ src/main/java/com/roobi/pulpy/FirstAuthPulpy.java
@@ -31,6 +31,7 @@ import org.apache.http.HttpResponse;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.scribe.builder.ServiceBuilder;
 import org.scribe.builder.api.FlickrApi;
@@ -98,7 +99,7 @@ public class FirstAuthPulpy extends HttpServlet {
 			  con = (Connection) DriverManager.getConnection(config.get("URL"),config.get("USER"),config.get("PASS"));
 	             PreparedStatement st=null;
 	             st=con.prepareStatement("insert into authen1(id,appname,descr,auth,rf,rmethod,a1,a2,b1,b2,b3,b4,h1,hv1,h2,hv2,h3,hv3,h4,hv4,h5,hv5,cname,ckey,csecname,cseckey,sname,svalue,aurl,tokenurl,tlabel,treplace,el,ev,osmeth,oreq,ockey,oskey,ourl1,ourl2,ourl3,sigckey,sigskey,sigmeth,logo) values ('"+id+"','"+appname+"','"+descr+"','"+authen+"','"+select1+"','"+select2+"','"+a1+"','"+a2+"','"+b1+"','"+b2+"','"+b3+"','"+b4+"','"+h1+"','"+hv1+"','"+h2+"','"+hv2+"','"+h3+"','"+hv3+"','"+h4+"','"+hv4+"','"+h5+"','"+hv5+"','"+cname+"','"+ckey+"','"+csecname+"','"+cseckey+"','"+sname+"','"+svalue+"','"+aurl+"','"+tokenurl+"','"+tlabel+"','"+treplace+"','"+el+"','"+ev+"','"+osmeth+"','"+oreq+"','"+ockey+"','"+oskey+"','"+ourl1+"','"+ourl2+"','"+ourl3+"','"+sigckey+"','"+sigskey+"','"+sigmeth+"',?)");				 
-	             st.setBlob(1, inputStream);
+	             st.setBlob(0, inputStream);
 	             st.executeUpdate();
 			     st.close();
 			   
@@ -147,24 +148,23 @@ public class FirstAuthPulpy extends HttpServlet {
 	                   }
 	                 session.setAttribute("oauth_signature1", oauth_signature1);
 	                    session.setAttribute("parameter_string", parameter_string);
-	                  String authorization_header_string = "OAuth oauth_consumer_key=\"" + oauth_consumer_key + "\",oauth_signature_method=\"HMAC-SHA1\",oauth_timestamp=\"" + 
-	                         oauth_timestamp + "\",oauth_nonce=\"" + oauth_nonce + "\",oauth_version=\"1.0\",oauth_signature=\"" + URLEncoder.encode(oauth_signature, "UTF-8") + "\"";
-	                  // System.out.println("authorization_header_string=" + authorization_header_string);
+	                    String authorization_header_string = "OAuth oauth_consumer_key=\"" + oauth_consumer_key + "\","
+	                     		+ "oauth_nonce=\"" + oauth_nonce + "\",oauth_signature_method=\"HMAC-SHA1\",oauth_signature=\"" + URLEncoder.encode(oauth_signature, "UTF-8") + "\",oauth_timestamp=\"" + 
+	                            oauth_timestamp + "\",oauth_version=\"1.0\"";
 	                   String uurl=url1+"?"+parameter_string+"&oauth_signature="+URLEncoder.encode(oauth_signature, "UTF-8");
-	                   System.out.println(uurl);
+	                   //System.out.println(uurl);
 	                  String oauth_token = "";
 	                 HttpClient httpclient = new DefaultHttpClient();
+	                 HttpResponse response1=null;
 	                    try {
+	                    	if(oreq1.equals("GET")){
 	                 	   HttpGet get1=new HttpGet(uurl);
-	                 	   HttpResponse response1=httpclient.execute(get1);
-	                     /* HttpPost httppost = new HttpPost(url1);
-	                         httppost.setHeader("Authorization",authorization_header_string);
-	                         ResponseHandler<String> responseHandler = new BasicResponseHandler();
-	                        String responseBody = httpclient.execute(httppost, responseHandler);
-	                       oauth_token = responseBody.substring(responseBody.indexOf("oauth_token=") + 12, responseBody.indexOf("&oauth_token_secret="));
-	                         System.out.println(responseBody); // */
-	                 	   System.out.println("Response Code : " + 
-	                                response1.getStatusLine().getStatusCode());
+	                 	    response1=httpclient.execute(get1);}
+	                    	else if(oreq1.equals("POST")){
+	                      HttpPost post = new HttpPost(url1);
+	                      post.setHeader("Authorization", authorization_header_string);
+	      				 response1 = httpclient.execute(post);
+	                    	}
 	          
 	         		BufferedReader rd = new BufferedReader(
 	                                new InputStreamReader(response1.getEntity().getContent()));
