01254313f269cb455a7910cb441f9182815aed51
susee <suseemukil@gmail.com>
2014-11-10 17:17:54 +0530

    commit

--- src/main/java/com/roobi/pulpy/AuthPulpy.java
+++ src/main/java/com/roobi/pulpy/AuthPulpy.java
@@ -706,30 +706,13 @@ public class AuthPulpy extends HttpServlet {
                   connection.setDoInput(true);
 	              connection.setRequestMethod("GET");
 	              String encoding=null;
-	            	 if(!"null".equals(b2) || !"".equals(b2) && "null".equals(b4) || "".equals(b4)){
+	            	 if(!"".equals(b2) && !"".equals(b4)){
 	            		 encoding = new String(
 	                    		 org.apache.commons.codec.binary.Base64.encodeBase64   
 	                    		    (org.apache.commons.codec.binary.StringUtils.getBytesUtf8(b2+":"+""))
 	                    		  );
 	   	              connection.setRequestProperty  ("Authorization", "Basic " + encoding);
 
-	            	 }
-	            	 else if(!"null".equals(b4) || !"".equals(b4) && "null".equals(b2) || "".equals(b2)){encoding = new String(
-        		 org.apache.commons.codec.binary.Base64.encodeBase64   
-     		    (org.apache.commons.codec.binary.StringUtils.getBytesUtf8(""+":"+b4))
-     		  );	              
-	            	 connection.setRequestProperty  ("Authorization", "Basic " + encoding);
-}
-	            	 else if(!"null".equals(b2) || !"".equals(b2) && !"null".equals(b4) || !"".equals(b2)){
-	            		 encoding = new String(
-	                    		 org.apache.commons.codec.binary.Base64.encodeBase64   
-	                    		    (org.apache.commons.codec.binary.StringUtils.getBytesUtf8(b2+":"+b4))
-	                    		  );
-	   	              connection.setRequestProperty  ("Authorization", "Basic " + encoding);
-
-	   	            	 } // else if encoding
-	            	 else if("null".equals(b2) && "null".equals(b4)){
-	            		 encoding=null;
 	            	 }
 	            	 
 	              if(!"null".equals(h1) && !"null".equals(h2) && !"null".equals(h3) && !"null".equals(h4) && !"null".equals(h5)){
