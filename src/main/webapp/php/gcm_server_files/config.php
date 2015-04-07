b00af868e5a52722ede6ebb593a4fe4fa999a256
susee <susee@minddotss.com>
2015-04-07 12:52:11 +0530

    inserted

---
+++ src/main/webapp/php/gcm_server_files/config.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Database config variables
+ */
+define("DB_HOST", "127.6.250.130:3306");
+define("DB_USER", "adminPQ1iFfN");
+define("DB_PASSWORD", "J5JhBL-XC9NG");
+define("DB_DATABASE", "mpulpy");
+
+/*
+ * Google Cloud Messaging API Key
+ */
+define("GOOGLE_API_KEY", "AIzaSyBxLeAF8t8wihNdsWhKjK88M897Ou5gP-E"); // Place your Google API Key
+                          
+?>
