1a8ee9ac38197a6028052f20600943c7f9940b02
susee <susee@minddotss.com>
2015-05-07 15:38:44 +0530

    check

--- src/main/java/com/roobi/pulpy/RestSwagger.java
+++ src/main/java/com/roobi/pulpy/RestSwagger.java
@@ -4,9 +4,14 @@ import org.json.JSONObject;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
 
 /**
  * Created by susee on 5/5/15.
@@ -15,11 +20,11 @@ import javax.ws.rs.core.Response;
 public class RestSwagger {
     @GET
     @Produces(MediaType.TEXT_PLAIN)
-    public Response getdata(){
-        String result="{\n" +
+    public Response getdata(@PathParam("usrid") String usrid) {
+        String result = "{\n" +
                 "  \"swaggerVersion\": \"1.2\",\n" +
                 "  \"basePath\": \"http://mindapp-pulpy.rhcloud.com\",\n" +
-                "\"tags\":{\"name\":\"mind\",\"description\":\"Get simplified data using appid\"},"+
+                "\"tags\":{\"name\":\"mind\",\"description\":\"Get simplified data using appid\"}," +
                 "  \"apis\": [\n" +
                 "    {\n" +
                 "       \"tags\":[  \n" +
@@ -58,10 +63,84 @@ public class RestSwagger {
                 "  ],\n" +
                 "  \"models\": {}\n" +
                 "}";
-        JSONObject object=new JSONObject();
-        object.put("","");
+        String output = "{\n" +
+                "  \"swaggerVersion\": \"1.2\",\n" +
+                "  \"basePath\": \"http://mindapp-pulpy.rhcloud.com\",\n" +
+                "\"tags\":{\"name\":\"mind\",\"description\":\"Get simplified data using appid\"}," +
+                "  \"apis\": [\n" +
+                "    {\n" +
+                "       \"tags\":[  \n" +
+                "               \"mind\"\n" +
+                "            ]," +
+                "      \"path\": \"/AuthXmlPulpy\",\n" +
+                "      \"operations\": [\n";
+        try {
+            Class.forName("com.mysql.jdbc.Driver").newInstance();
+            java.sql.Connection con = DriverManager.getConnection("jdbc:mysql://127.6.250.130:3306/mpulpy", "adminPQ1iFfN", "J5JhBL-XC9NG");
+            PreparedStatement st = con.prepareStatement("SELECT appid From xmlconfig where id='" + usrid + "'");
+            ResultSet rs = st.executeQuery();
+            ArrayList<String> arrapid = new ArrayList<String>();
+            if (rs != null) {
+                while (rs.next()) {
+                    String apid = rs.getString("appid");
+                    arrapid.add(apid);
+                }
+            }
 
-        return Response.ok(result)
+            for (int i = 0; i < arrapid.size(); i++) {
+                PreparedStatement st1 = con.prepareStatement("SELECT * From config where appid='" + arrapid.get(i) + "'");
+                ResultSet rs1 = st1.executeQuery();
+                ArrayList<String> parm = new ArrayList<String>();
+                output += "        {\n" +
+                        "          \"method\": \"GET\",\n" +
+                        "          \"summary\": \"Mindpulpy simplified result for Appid " + arrapid.get(i) + "\",\n" +
+                        "          \"type\": \"string\",\n" +
+                        "          \"nickname\": \"Simplified result\",\n" +
+                        "          \"produces\":[  \n" +
+                        "               \"application/xml\",\n" +
+                        "               \"application/json\"\n" +
+                        "            ]," +
+                        "          \"parameters\": [\n" +
+                        "            {\n" +
+                        "              \"name\": \"appid\",\n" +
+                        "              \"value\": \"" + arrapid.get(i) + "\",\n" +
+                        "              \"description\": \"Enter your unique appid.\",\n" +
+                        "              \"required\": true,\n" +
+                        "              \"type\": \"string\",\n" +
+                        "              \"paramType\": \"query\"\n" +
+                        "            },\n";
+
+                if (rs1 != null) {
+                    while (rs1.next()) {
+                        for (int n = 1; n < 11; n++) {
+                            String par = "" + rs1.getString("p" + n);
+                            if (!par.equals("null")) {
+                                parm.add(par);
+                                output += "            {\n" +
+                                        "              \"name\": \"p" + n + "\",\n" +
+                                        "              \"description\": \"Enter " + par + ".\",\n" +
+                                        "              \"required\": true,\n" +
+                                        "              \"type\": \"string\",\n" +
+                                        "              \"paramType\": \"query\"\n" +
+                                        "            },\n";
+                            }
+                        }
+
+                    }
+                }
+                output += "          ]\n" +
+                        "        }\n";
+            }
+            output += "      ]\n" +
+                    "    }\n" +
+                    "  ],\n" +
+                    "  \"models\": {}\n" +
+                    "}";
+        }//try
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+        return Response.ok(output)
                 .header("Access-Control-Allow-Origin", "*")
                 .header("Access-Control-Allow-Methods", "POST, GET, PUT, UPDATE, OPTIONS")
                 .header("Access-Control-Allow-Headers", "Content-Type, Accept, X-Requested-With").build();
